83b37e416da51500d9b592de4b4d4766def7b914
patrickmj <patrickmjchnm@gmail.com>
2016-07-27 19:48:30 -0400

    save site-specific settings

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -14,8 +14,19 @@ class IndexController extends AbstractActionController
         $siteSettings = $this->siteSettings();
         $view = new ViewModel;
         $form = $this->getForm(Form::class);
+        if ($this->getRequest()->isPost()) {
+            $params = $this->params()->fromPost();
+            if (isset($params['propertyIds'])) {
+                $propertyIds = json_encode($params['propertyIds']);
+            } else {
+                $propertyIds = json_encode(array());
+            }
+            $siteSettings->set('metadata_browse_properties', $propertyIds);
+        }
         
+        $filteredPropertyIds = $siteSettings->get('metadata_browse_properties');
         $view->setVariable('form', $form);
+        $view->setVariable('filteredPropertyIds', $filteredPropertyIds);
         return $view;
     }
 }
