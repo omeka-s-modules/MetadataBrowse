f8b05a0d26eb77ae466288353da0d79892915468
patrickmj <patrickmjchnm@gmail.com>
2016-07-27 17:25:00 -0400

    add route and nave to site settings

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,63 @@
 <?php
-return array(
-    'view_manager' => array(
-        'template_path_stack'      => array(
+return [
+    'view_manager' => [
+        'template_path_stack'      => [
             OMEKA_PATH . '/modules/MetadataBrowse/view',
-        ),
-    ),
-);
+        ],
+    ],
+    'navigation' => [
+        'site' => [
+            [
+                'label' => 'Metadata Browse', // @translate
+                'route' => 'admin/site/slug/metadata-browse/default',
+                'action' => 'index',
+                'useRouteMatch' => true,
+                'pages' => [
+                    [
+                        'route' => 'admin/site/slug/metadata-browse/default',
+                        'visible' => false,
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'admin' => [
+                'child_routes' => [
+                    'site' => [
+                        'child_routes' => [
+                            'slug' => [
+                                'child_routes' => [
+                                    'metadata-browse' => [
+                                        'type' => 'Literal',
+                                        'options' => [
+                                            'route' => '/metadata- browse',
+                                            'defaults' => [
+                                                '__NAMESPACE__' => 'MetadataBrowse\Controller',
+                                                'controller' => 'index',
+                                                'action' => 'index',
+                                            ],
+                                        ],
+                                        'may_terminate' => true,
+                                        'child_routes' => [
+                                            'default' => [
+                                                'type' => 'Segment',
+                                                'options' => [
+                                                    'route' => '/:action',
+                                                    'constraints' => [
+                                                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                                    ],
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ]
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
