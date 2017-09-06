6ad155e0c3c8d6eb3dbeebad728715939ffb2c27
patrickmj <patrickmjchnm@gmail.com>
2017-09-06 13:33:31 -0400

    update translations

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -15,7 +15,6 @@ class ConfigForm extends Form
             'name' => 'metadata_browse_use_globals',
             'options' => array(
                         'label' => 'Use global configuration on admin side', // @translate
-                        //'info'  => "If checked, the properties selected below will be linked on the admin side, overriding all site-specific settings. Each site's own settings will be reflected on the public side. Otherwise, the admin side will reflect the aggregated settings for all sites; anything selected to be a link in any site will be a link on the admin side.", // @translate
                     ),
             'attributes' => array(
                         'checked' => $this->globalSettings->get('metadata_browse_use_globals') ? 'checked' : '',
